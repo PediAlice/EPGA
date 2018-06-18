ea76162f42c61c057270f16f3a612662b099fc60
bolsonaroPresidente <yoshiaudrin6@gmail.com>
2018-06-18 16:29:01 -0300

    Iniciando projeto cga

---
+++ src/main/java/com/epga/cga/CgaApplication.java
@@ -0,0 +1,12 @@
+package com.epga.cga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epga/cga/CgaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epga.cga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CgaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
